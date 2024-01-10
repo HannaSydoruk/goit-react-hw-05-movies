6cf71a93981bbe5472c2e5631b4307400209beab
HannaSydoruk <hanna.sydoruk@gmail.com>
2024-01-10 15:30:47 +0200

    some pages added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import { NavLink, Routes, Route } from 'react-router-dom';
+import css from './App.module.css';
+import Home from 'pages/home';
+import Movies from 'pages/movies';
+import MovieDetails from 'pages/movieDetails';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+        <NavLink
+          className={({ isActive }) =>
+            `${css.navLink} ${isActive ? css.active : ''}`
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `${css.navLink} ${isActive ? css.active : ''}`
+          }
+          to="/movies"
+          end
+        >
+          Movies
+        </NavLink>
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+        </Routes>
+      </main>
     </div>
   );
 };
